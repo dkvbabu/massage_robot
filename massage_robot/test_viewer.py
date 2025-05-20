0d02ccfb0bc70477867c3874c4aba01cca620007
engyasin <15736151+engyasin@users.noreply.github.com>
2025-05-20 15:11:19 +0200

    add cameras

--- massage_robot/test_viewer.py
+++ massage_robot/test_viewer.py
@@ -2,6 +2,7 @@ import pybullet as p
 import time,os
 import pybullet_data
 import numpy as np
+import cv2
 
 import configparser
 
@@ -31,6 +32,25 @@ def draw_data(Forces,armparts,bodyparts):
     plt.title('Body Part')
     plt.plot(bodyparts)
     plt.show()
+
+
+
+def arm_camera(projection_matrix,armId=2):
+
+    # Center of mass position and orientation (of link-6)
+    com_p, com_o, _, _, _, _ = p.getLinkState(armId, 4)
+    rot_matrix = p.getMatrixFromQuaternion(com_o)
+    rot_matrix = np.array(rot_matrix).reshape(3, 3)
+    # Initial vectors
+    init_camera_vector = (0, 0, 1) # z-axis
+    init_up_vector = (0, 1, 0) # y-axis
+    # Rotated vectors
+    camera_vector = rot_matrix.dot(init_camera_vector)
+    up_vector = rot_matrix.dot(init_up_vector)
+    view_matrix = p.computeViewMatrix(com_p, com_p + 0.1 * camera_vector, up_vector)
+    img = p.getCameraImage(1000, 1000, view_matrix, projection_matrix)
+
+    return np.array(img[2]).reshape(1000,1000,-1)
 
 
 
@@ -100,7 +120,30 @@ def main():
     p.configureDebugVisualizer(p.COV_ENABLE_SHADOWS, 0)
     p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
 
+
+    # Good Cam 1
+    #(1024, 768, (-0.9937682151794434, -0.07253935188055038, 0.08463338017463684, 0.0, 0.1114664301276207, -0.6467175483703613, 0.754540741443634, 0.0, 0.0, 0.7592723965644836, 0.6507730484008789, 0.0, -0.18283233046531677, -0.3637703061103821, -1.4271166324615479, 1.0), (0.7499999403953552, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, -1.0000200271606445, -1.0, 0.0, 0.0, -0.02000020071864128, 0.0), (0.0, 0.0, 1.0), (-0.08463338017463684, -0.754540741443634, -0.6507730484008789), (-26500.486328125, 2972.43798828125, 0.0), (-1450.786865234375, -12934.3505859375, 15185.4462890625), 173.6001434326172, -40.599910736083984, 1.200002670288086, (-0.18885917961597443, -0.04351018741726875, 0.42400023341178894))
+
+    # Good Cam 2
+    #(1024, 768, (-0.041878849267959595, -0.6659466028213501, 0.7448229789733887, 0.0, 0.9991226196289062, -0.027913566678762436, 0.0312197208404541, 0.0, 0.0, 0.7454769015312195, 0.6665313839912415, 0.0, -0.14472033083438873, -0.7917931079864502, -1.0678036212921143, 1.0), (0.7499999403953552, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, -1.0000200271606445, -1.0, 0.0, 0.0, -0.02000020071864128, 0.0), (0.0, 0.0, 1.0), (-0.7448229789733887, -0.0312197208404541, -0.6665313839912415), (-1116.7694091796875, 26643.2734375, 0.0), (-13318.9326171875, -558.2713623046875, 14909.5400390625), 92.40018463134766, -41.799922943115234, 1.0, (-0.4828510880470276, 0.12460841238498688, 0.6354567408561707))
+
+
+
+
     human_inst = load_scene(physicsClient)
+
+    # Reset the camera
+    p.resetDebugVisualizerCamera(
+        cameraYaw= 173.6001434326172,
+        cameraPitch=-40.599910736083984,
+        cameraDistance=1.200002670288086,
+        cameraTargetPosition=(-0.18885917961597443, -0.04351018741726875, 0.42400023341178894),
+    )
+    p.resetDebugVisualizerCamera(cameraYaw= 92.4,cameraPitch=-41.8,cameraDistance=1.0,cameraTargetPosition=(-0.4828510, 0.12460, 0.6354567),)
+
+    cam1_vmat = (-0.9937682151794434, -0.07253935188055038, 0.08463338017463684, 0.0, 0.1114664301276207, -0.6467175483703613, 0.754540741443634, 0.0, 0.0, 0.7592723965644836, 0.6507730484008789, 0.0, -0.18283233046531677, -0.3637703061103821, -1.4271166324615479, 1.0)
+    cam1_pmat = (0.7499999403953552, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, -1.0000200271606445, -1.0, 0.0, 0.0, -0.02000020071864128, 0.0)
+
 
     # Setup Path
     nArmJoints = p.getNumJoints(armId, physicsClientId=physicsClient)
@@ -111,6 +154,7 @@ def main():
 
     pntsAndReturn = np.vstack((pnts,pnts[::-1]))
     print(f'Number of DOFs: {nArmJoints})')
+
 
     #breakpoint()
     #print(p.getAABB(human_inst.body))
@@ -145,6 +189,17 @@ def main():
         p.setJointMotorControlArray(armId, jointIndices=range(1,nArmJoints-3), controlMode=p.POSITION_CONTROL, 
                                     targetPositions=JointPoses,forces=100*np.ones_like(JointPoses))
         time.sleep(TimeStep)
+        # Get the current camera
+        cam = p.getDebugVisualizerCamera()
+
+
+        # try hide arm with  changeVisualShape
+        #print(cam)
+        img_out = p.getCameraImage(640, 480, cam1_vmat, cam1_pmat)
+        img = np.array(img_out[2]).reshape(480, 640,-1)[:,:,:3].astype(np.uint8)# RGBA
+        #img = arm_camera(projection_matrix,armId=armId)
+        cv2.imshow('img',img)
+        cv2.waitKey(1)
 
         if j%int(2/TimeStep):
             # Update Path
@@ -153,6 +208,7 @@ def main():
             pntsAndReturn = np.vstack((pnts,pnts[::-1]))
 
 
+    cv2.destroyAllWindows()
 
     armPos, armOrn = p.getBasePositionAndOrientation(armId)
 
